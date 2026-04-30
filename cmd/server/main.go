ff29a03fb090cfb971c763d250a327508a3967c3
Voravee <voravee.nuc@dome.tu.ac.th>
2026-04-30 23:41:04 +0700

    feat: setup server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/api/doctors", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "ok"})
+	})
+
+	r.Run(":8080")
+}
