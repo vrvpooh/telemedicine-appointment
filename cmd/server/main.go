3a76260aa29de94f69db4a6d1565e5b65579e915
Kasidit Kongprapan-6609650129 <kasidit.kon@dome.tu.ac.th>
2026-05-04 21:59:26 +0700

    feat: implement notification and feedback

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -24,12 +24,23 @@ func main() {
 	doctorService := &service.DoctorService{Repo: doctorRepo}
 	handler.SetupDoctorHandler(doctorService)
 
+	// Setup Notification
+    noteRepo := &repository.NotificationRepository{}
+    noteService := &service.NotificationService{Repo: noteRepo}
+    handler.SetupNotificationHandler(noteService)
+
+    // Setup Feedback & Admin Verify
+    feedRepo := &repository.FeedbackRepository{}
+    feedService := &service.FeedbackService{Repo: feedRepo}
+    handler.SetupFeedbackHandler(feedService)
+	
 	// Gin
 	r := gin.Default()
 
 	// Register routes
 	route.RegisterSlotRoutes(r)
 	route.RegisterDoctorRoutes(r)
+	route.RegisterNotificationRoutes(r)
 
 	r.Run(":8080")
 }
