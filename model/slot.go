fe14d308a5bcfa33cb76db46fb6638ed8b2f207e
NonnaritRammaneekultawat-6609650459 <146455311+NonnaritRammaneekultawat-6609650459@users.noreply.github.com>
2026-05-04 23:35:42 +0700

    Revert "feat: implement records API"

--- model/slot.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Slot struct {
-	ID        int64  `json:"id"`
-	DoctorID  int64  `json:"doctor_id"`
-	StartTime string `json:"start_time"`
-	EndTime   string `json:"end_time"`
-	IsBooked  bool   `json:"is_booked"`
-}
